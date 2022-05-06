e9bd0dcebc3e0ba5a35f19b5eeafc0d8e7cc9a68
Thomas Bergamin <thomas@macbook-air-de-thomas.home>
2022-05-06 18:24:33 +0200

    feat: 🎸 init home page and stats page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,97 @@
+import {
+  Box,
+  Heading,
+  VStack,
+  SlideFade,
+  Container,
+  FormControl,
+  HStack,
+  FormLabel,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+import { Footer } from '../components/Footer/Footer';
 import { Navbar } from '../components/Navbar/Navbar/Navbar';
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Container maxW="120ch" py="3" px={4}>
+        <SlideFade in offsetY="50px">
+          <Box as="section" minHeight="70vh">
+            <VStack
+              alignItems="center"
+              gap={10}
+              textAlign="center"
+              // mt={[24, 48]}
+              mx={4}
+            >
+              <Heading as="h1" textColor="gray.100" size="3xl">
+                Bienvenue sur ScrabbleCount 👋
+              </Heading>
+              <Heading as="h2" textColor="gray.100" size="xl">
+                Ajoute ta partie ici ✏
+              </Heading>
+              <Box
+                bgColor="gray.600"
+                p="8"
+                borderRadius="10"
+                w={['100%', '100%', '50%']}
+                /* filter="blur(3px)"  */
+              >
+                <FormControl>
+                  <FormLabel htmlFor="date">Date</FormLabel>
+                  <Input id="date" type="date" />
+                  <FormLabel mt="4" htmlFor="time">
+                    Heure
+                  </FormLabel>
+                  <Input id="time" type="time" />
+                  <FormLabel mt="4" htmlFor="location">
+                    Lieu
+                  </FormLabel>
+                  <Input id="location" type="text" />
+
+                  <HStack mt="4">
+                    <VStack spacing="1" alignItems="baseline">
+                      <FormLabel htmlFor="player1">Joueur 1</FormLabel>
+                      <Input id="player1" type="text" placeholder="Joueur 1" />
+                    </VStack>
+                    <VStack spacing="1" alignItems="baseline">
+                      <FormLabel htmlFor="score1">Score</FormLabel>
+                      <Input id="score1" type="number" min="0" />
+                    </VStack>
+                    <VStack spacing="1" alignItems="baseline">
+                      <FormLabel htmlFor="scrabbles1">Scrabbles</FormLabel>
+                      <Input id="scrabbles1" type="number" min="0" />
+                    </VStack>
+                  </HStack>
+                  <HStack mt="4">
+                    <VStack spacing="1" alignItems="baseline">
+                      <FormLabel htmlFor="player2">Joueur 2</FormLabel>
+                      <Input id="player2" type="text" placeholder="Joueur 2" />
+                    </VStack>
+                    <VStack spacing="1" alignItems="baseline">
+                      <FormLabel htmlFor="score2">Score</FormLabel>
+                      <Input id="score2" type="number" min="0" />
+                    </VStack>
+                    <VStack spacing="1" alignItems="baseline">
+                      <FormLabel htmlFor="scrabbles2">Scrabbles</FormLabel>
+                      <Input id="scrabbles2" type="number" min="0" />
+                    </VStack>
+                  </HStack>
+                </FormControl>
+                <Button mt={6} colorScheme="teal" type="submit" w="100%">
+                  Submit
+                </Button>
+              </Box>
+            </VStack>
+          </Box>
+        </SlideFade>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,3 +1,22 @@
+import {
+  Heading,
+  Spacer,
+  Flex,
+  Container,
+  Box,
+  Stat,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Table,
+  Tr,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+} from '@chakra-ui/react';
 import { Footer } from '../components/Footer/Footer';
 import { Navbar } from '../components/Navbar/Navbar/Navbar';
 
@@ -5,6 +24,64 @@ const Stats = () => {
   return (
     <>
       <Navbar />
+      <Container maxW="120ch" py="3">
+        <Flex>
+          <Heading as="h2" textColor="gray.50" size="xl">
+            Statistiques
+          </Heading>
+          <Spacer></Spacer>
+          <Heading as="h3" textColor="gray.50" size="l">
+            Le 06 Mai à 12:23
+          </Heading>
+        </Flex>
+
+        <Box mt="4" width="100%" borderRadius="10" bgColor="teal.400" p="6">
+          <StatGroup color="whiteAlpha.900" height="100%html">
+            <Stat>
+              <StatLabel>Meilleur Joueur</StatLabel>
+              <StatNumber>Cécile</StatNumber>
+              <StatHelpText>25 parties gagnées</StatHelpText>
+            </Stat>
+            <Spacer></Spacer>
+            <Stat borderLeft="solid 2px white" pl="4">
+              <StatLabel>Meilleur Score</StatLabel>
+              <StatNumber>535</StatNumber>
+              <StatHelpText>Christophe, le 05 Mai 2022</StatHelpText>
+            </Stat>
+            {/* links to the game where the score was made */}
+            <Spacer></Spacer>
+            <Stat borderLeft="solid 2px white" pl="4">
+              <StatLabel>Plus de Srabbles en une partie</StatLabel>
+              <StatNumber>6</StatNumber>
+              <StatHelpText>Cécile, le 05 Mai 2022</StatHelpText>
+            </Stat>
+          </StatGroup>
+        </Box>
+
+        <TableContainer my="6">
+          <Table colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>To convert</Th>
+                <Th>into</Th>
+                <Th isNumeric>multiply by</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>inches</Td>
+                <Td>millimetres (mm)</Td>
+                <Td isNumeric>25.4</Td>
+              </Tr>
+              <Tr>
+                <Td>feet</Td>
+                <Td>centimetres (cm)</Td>
+                <Td isNumeric>30.48</Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Container>
       <Footer />
     </>
   );
